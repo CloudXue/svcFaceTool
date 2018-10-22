4b572f89f5418a20c905a11288225be49ca8f07d
lyd <ai_tiny@163.com>
2018-10-22 17:11:46 +0800

    【DB代码生成】 表信息查询-dto继承Serializable

--- src/main/java/bean/gencode/adapter/DtoClassAdapter.java
+++ src/main/java/bean/gencode/adapter/DtoClassAdapter.java
@@ -38,7 +38,7 @@ public class DtoClassAdapter extends ClassInfoAdapter implements IClassToFile {
                 }
             }
         }
-
+        classInfo.getInterfaceList().add("java.io.Serializable");
         classInfo.setType(ClassInfo.TYPE_CLASS);
         //生成set、get方法
         initMethod();
